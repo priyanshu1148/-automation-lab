d659761f665be159f800648d7553602bb2751701
Priyanshu Yadav <priyanshu.automation.lab@gmail.com>
2026-07-24 19:57:14 +0530

    Add Microsoft Clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,6 +47,20 @@ export default function RootLayout({
             gtag('config', 'G-HJ8ZG808WH');
           `}
         </Script>
+
+        {/* Microsoft Clarity */}
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);
+              t.async=1;
+              t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];
+              y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "xrhx4bwy6m");
+          `}
+        </Script>
       </body>
     </html>
   );
